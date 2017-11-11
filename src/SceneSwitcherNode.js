43b59217b8fbc909b9e147e3080de288b44ecc7f
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-11-11 19:46:57 +0100

    Update music to new draft

--- src/SceneSwitcherNode.js
+++ src/SceneSwitcherNode.js
@@ -37,25 +37,26 @@
       this.inputs.L.enabled = false;
 
       let selectedScene;
-      if (BEAN < 48 * 8) {
+      const offsettedBean = BEAN - PROJECT.music.BEANOffset;
+      if (offsettedBean < 48 * 8) {
         selectedScene = this.inputs.A;
-      } else if (BEAN < 48 * 16) {
+      } else if (offsettedBean < 48 * 16) {
         selectedScene = this.inputs.B;
-      } else if (BEAN < 48 * 24) {
+      } else if (offsettedBean < 48 * 24) {
         selectedScene = this.inputs.C;
-      } else if (BEAN < 48 * 32) {
+      } else if (offsettedBean < 48 * 32) {
         selectedScene = this.inputs.E;
-      } else if (BEAN < 48 * 40) {
+      } else if (offsettedBean < 48 * 40) {
         selectedScene = this.inputs.F;
-      } else if (BEAN < 48 * 48) {
+      } else if (offsettedBean < 48 * 48) {
         selectedScene = this.inputs.G;
-      } else if (BEAN < 48 * 56) {
+      } else if (offsettedBean < 48 * 56) {
         selectedScene = this.inputs.H;
-      } else if (BEAN < 48 * 64) {
+      } else if (offsettedBean < 48 * 64) {
         selectedScene = this.inputs.I;
-      } else if (BEAN < 48 * 72) {
+      } else if (offsettedBean < 48 * 72) {
         selectedScene = this.inputs.J;
-      } else if (BEAN < 48 * 80) {
+      } else if (offsettedBean < 48 * 80) {
         selectedScene = this.inputs.K;
       } else {
         selectedScene = this.inputs.L;
