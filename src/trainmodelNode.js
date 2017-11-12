1967cd61f4b508883a4a848c3de441d3203679dc
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-11-12 22:46:52 +0100

    Start working on twister inside train

--- src/trainmodelNode.js
+++ src/trainmodelNode.js
@@ -3,6 +3,19 @@
     constructor(id, options) {
       super(id, options);
       this.snareThrob = 0;
+
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      canvas.width = 512;
+      canvas.height = 256;
+      ctx.fillStyle = '#00e04f';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.font = '70pt Arial';
+      ctx.fillStyle = 'rgb(55, 60, 63)';
+      ctx.fillText('REVISION', 256, 128);
+      this.revisionTexture = new THREE.CanvasTexture(canvas);
     }
 
     update(frame) {
@@ -12,14 +25,15 @@
         this.snareThrob = 1;
       }
       this.uniforms.snareThrob.value = this.snareThrob;
+      this.uniforms.tDiffuse.value = this.revisionTexture;
     }
 
     resize() {
       let width = 16 * GU;
       let height = 9 * GU;
-      if(width >= 1601) {
-        width /= 2;
-        height /= 2;
+      if(width >= 1280) {
+        width = 1280;
+        height = 720;
       }
       this.renderTarget.setSize(width, height);
     }
