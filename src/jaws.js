49033036f7386ded5dc0f61871fbfaed064a6032
Stian Jensen <me@stianj.com>
2017-11-12 20:56:09 +0100

    Add chroma transition shader with example

--- src/jaws.js
+++ src/jaws.js
@@ -34,7 +34,7 @@
       this.ctx.scale(GU, GU);
 
       this.ctx.translate(8, 4.5);
-      const t = smoothstep(0, 1, (this.frame - FRAME_FOR_BEAN(3552) + 20) / 20);
+      const t = smoothstep(0, 1, (this.frame - FRAME_FOR_BEAN(68 * 12 * 4) + 20) / 20);
       const angle = lerp(0, Math.PI, t);
       this.ctx.rotate(angle);
       const scale = 1 + (t * (1 - t)) * 8;
@@ -44,23 +44,23 @@
       this.ctx.lineWidth = 0.2;
 
       const timings = {
-        0: FRAME_FOR_BEAN(3456),
-        2: FRAME_FOR_BEAN(3465),
-        4: FRAME_FOR_BEAN(3480),
-        6: FRAME_FOR_BEAN(3490),
-        8: FRAME_FOR_BEAN(3504),
-        10: FRAME_FOR_BEAN(3514),
-        12: FRAME_FOR_BEAN(3528),
-        14: FRAME_FOR_BEAN(3539),
+        0: FRAME_FOR_BEAN(66 * 12 * 4),
+        2: FRAME_FOR_BEAN(66 * 12 * 4 + 9),
+        4: FRAME_FOR_BEAN(66 * 12 * 4 + 24),
+        6: FRAME_FOR_BEAN(66 * 12 * 4 + 34),
+        8: FRAME_FOR_BEAN(67 * 12 * 4),
+        10: FRAME_FOR_BEAN(67 * 12 * 4 + 10),
+        12: FRAME_FOR_BEAN(67 * 12 * 4 + 24),
+        14: FRAME_FOR_BEAN(67 * 12 * 4 + 35),
 
-        16: FRAME_FOR_BEAN(3456 + 48 * 2),
-        18: FRAME_FOR_BEAN(3465 + 48 * 2),
-        20: FRAME_FOR_BEAN(3480 + 48 * 2),
-        22: FRAME_FOR_BEAN(3490 + 48 * 2),
-        24: FRAME_FOR_BEAN(3504 + 48 * 2),
-        26: FRAME_FOR_BEAN(3514 + 48 * 2),
-        28: FRAME_FOR_BEAN(3528 + 48 * 2),
-        30: FRAME_FOR_BEAN(3536 + 48 * 2),
+        16: FRAME_FOR_BEAN(66 * 12 * 4 + 48 * 2),
+        18: FRAME_FOR_BEAN(66 * 12 * 4 + 9 + 48 * 2),
+        20: FRAME_FOR_BEAN(66 * 12 * 4 + 24 + 48 * 2),
+        22: FRAME_FOR_BEAN(66 * 12 * 4 + 34 + 48 * 2),
+        24: FRAME_FOR_BEAN(67 * 12 * 4 + 48 * 2),
+        26: FRAME_FOR_BEAN(67 * 12 * 4 + 10 + 48 * 2),
+        28: FRAME_FOR_BEAN(67 * 12 * 4 + 24 + 48 * 2),
+        30: FRAME_FOR_BEAN(67 * 12 * 4 + 35 + 48 * 2),
       };
 
       for(let i = 0; i < 32; i+=2) {
@@ -71,7 +71,7 @@
         }
         const t = easeOut(1, 0, (this.frame - timings[i]) / 15);
         this.ctx.save();
-        this.ctx.translate(-(this.frame - 9015) / 30, (this.frame - 9015) / 100 + 6 * t +  i / 4 * t);
+        this.ctx.translate(-(this.frame - 8270) / 30, (this.frame - 8270) / 100 + 6 * t +  i / 4 * t);
         this.ctx.beginPath();
         this.ctx.moveTo(i, 9);
         this.ctx.lineTo(i + 2, 9 - (5 + i / 4));
