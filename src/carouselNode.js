e5b80dec55f57abf9bc2fb13cc94c10bcd36641d
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-11-19 18:11:16 +0100

    Add minor tweaks to carousel
    Nudge color values and remove branches in code.

--- src/carouselNode.js
+++ src/carouselNode.js
@@ -11,8 +11,8 @@
       const t = (frame - FRAME_FOR_BEAN(4 * 12 * 64)) / 60;
       this.uniforms.t.value = frame / 60;
       this.uniforms.divisions.value = 6;
-      this.uniforms.foregroundColor.value = new THREE.Vector4(1, .2, .1, 1);
-      this.uniforms.backgroundColor.value = new THREE.Vector4(1, .9, .6, 1);
+      this.uniforms.foregroundColor.value = new THREE.Color(1, 73 / 255, 130 / 255);
+      this.uniforms.backgroundColor.value = new THREE.Color(1, .9, .6);
       this.uniforms.radiusMultiplier.value = easeIn(0, 5, t * 0.05);
       this.uniforms.origo.value = new THREE.Vector2(easeOut(0, 0.5, t / 8),
                                                     easeOut(0, 0.5, t / 3));
