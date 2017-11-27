ebf22a670f97dee0bb0d5425e69706bda146a3ae
Rune Holmgren <Raane.Holm@gmail.com>
2017-11-27 01:34:58 +0100

    Initial work on the first few animations to the metatron scene.

--- src/metatron.js
+++ src/metatron.js
@@ -8,6 +8,13 @@
         }
       });
 
+
+
+      var zoom = 5;
+      this.camera = new THREE.OrthographicCamera( -zoom * 16, zoom * 16 , zoom * 9, -zoom * 9, 1, 100000 );
+      this.camera.position.z = 100;
+
+
       this.cube = new THREE.Mesh(new THREE.BoxGeometry(176, 99, 0.1),
                                  new THREE.MeshBasicMaterial({ color: 0x23132 }));
       this.cube.position.set(0,0,-10);
@@ -15,7 +22,7 @@
 
       var radius   = 30,
       segments = 64,
-      material = new THREE.LineDashedMaterial( { color: 0x999999 } ),
+      material = new THREE.LineDashedMaterial( { color: 0x222222 } ),
       geometry = new THREE.CircleGeometry( radius, segments );
 
       // Remove center vertex
@@ -26,7 +33,7 @@
 
       var radius2  = 36,
       segments2 = 6,
-      material2 = new THREE.LineBasicMaterial( { color: 0x999999 } ),
+      material2 = new THREE.LineBasicMaterial( { color: 0x222222 } ),
       geometry2 = new THREE.CircleGeometry( radius2, segments2 );
 
       geometry2.vertices.shift();
@@ -36,7 +43,7 @@
 
       var radius3  = 10,
       segments3 = 6,
-      material3 = new THREE.LineBasicMaterial( { color: 0x999999 } ),
+      material3 = new THREE.LineBasicMaterial( { color: 0x222222 } ),
       geometry3 = new THREE.CircleGeometry( radius3, segments3 );
 
       geometry3.vertices.shift();
@@ -57,12 +64,96 @@
       this.scene.add( hex3 );
       this.scene.add( hex4 );
 
-      this.camera.position.z = 100;
+ 
+      var line_material = new THREE.LineBasicMaterial( { color: 0x999999 } );
+      var small_radius  = 10;
+      var hex_segments = 6;
+      var small_geometry = new THREE.CircleGeometry( small_radius, hex_segments );
+
+      small_geometry.vertices.shift();
+      small_geometry.computeLineDistances();
+
+      this.center_rotation_container = new THREE.Object3D();
+
+      this.small_center_hex = new THREE.Line( small_geometry, line_material );
+      this.center_rotation_container.add(this.small_center_hex);
+      this.small_center_hex.rotation.set(0, 0, Math.PI / 6);
+
+      this.level1_hex1 = new THREE.Line( small_geometry, line_material );
+      this.level1_hex2 = new THREE.Line( small_geometry, line_material );
+      this.level1_hex3 = new THREE.Line( small_geometry, line_material );
+      this.center_rotation_container.add(this.level1_hex1);
+      this.center_rotation_container.add(this.level1_hex2);
+      this.center_rotation_container.add(this.level1_hex3);
+
+      this.level1_hex1.rotation.set(0, 0, Math.PI/6);
+      this.level1_hex2.rotation.set(0, 0, -Math.PI/6);
+      this.level1_hex3.rotation.set(0, 0, Math.PI/6);
+
+      this.scene.add(this.center_rotation_container);
+
+      var line_geometry = new THREE.Geometry();
+      line_geometry.vertices.push(new THREE.Vector3(0, -30, 0));
+      line_geometry.vertices.push(new THREE.Vector3(0, 30, 0));
+
+      this.center_line1 = new THREE.Line( line_geometry, line_material );
+      this.center_line2 = new THREE.Line( line_geometry, line_material );
+      this.center_line3 = new THREE.Line( line_geometry, line_material );
+
+      this.scene.add(this.center_line1);
+      this.scene.add(this.center_line2);
+      this.scene.add(this.center_line3);
+      
+      var circle_geometry = new THREE.CircleGeometry( 10, 128 );
+      circle_geometry.vertices.shift();
+      circle_geometry.computeLineDistances();
+      this.center_circle = new THREE.Line( circle_geometry, line_material );
+      this.scene.add(this.center_circle);
     }
 
     update(frame) {
       super.update(frame);
 
+      var asmoothstep = function (start_frame, duration, frame) {
+        return smoothstep(0, 1, (frame - start_frame) / duration);
+      }
+
+      // Square root of three divided by two. For a hex of diameter 1 this is the distance from the center to the edge.
+      var r32 = 0.86602540378;
+
+      this.level1_hex1.position.set(100, 0, 0);
+      this.level1_hex2.position.set(100, 0, 0);
+      this.level1_hex3.position.set(100, 0, 0);
+      this.center_rotation_container.rotation.set(0, 0, Math.PI/3);
+      this.center_line1.scale.set(0, 0, 0);
+      this.center_line2.scale.set(0, 0, 0);
+      this.center_line3.scale.set(0, 0, 0);
+      this.center_circle.scale.set(0, 0, 0);
+
+      if ( frame > FRAME_FOR_BEAN(22 * 48)) {
+        var scale = asmoothstep(FRAME_FOR_BEAN(22 * 48), FRAME_FOR_BEAN(48), frame)
+        this.small_center_hex.scale.set(scale, scale, scale);
+      } 
+      if (frame > FRAME_FOR_BEAN(23 * 48)) {
+        var progress = asmoothstep(FRAME_FOR_BEAN(23 * 48), FRAME_FOR_BEAN(48), frame);
+        this.level1_hex1.position.set(10 * r32 * progress, 10 / 2 * progress, 0);
+        this.level1_hex2.position.set(-10 * r32 * progress, 10 / 2 * progress, 0);
+        this.level1_hex3.position.set(0, -10 * progress, 0);
+
+        var progress2 = asmoothstep(FRAME_FOR_BEAN(23 * 48), FRAME_FOR_BEAN(24), frame);
+        this.center_line1.scale.set(progress2, progress2, progress2);
+        this.center_line2.scale.set(progress2, progress2, progress2);
+        this.center_line3.scale.set(progress2, progress2, progress2);
+
+        var progress3 = asmoothstep(FRAME_FOR_BEAN(23.5 * 48), FRAME_FOR_BEAN(24), frame);
+        this.center_line2.rotation.set(0, 0, progress3 * 2 * Math.PI / 3);
+        this.center_line3.rotation.set(0, 0, -progress3 * 2 * Math.PI / 3);
+
+        this.center_circle.scale.set(progress3 * 1.5, progress3 * 1.5, progress3 * 1.5);
+      }
+      if (frame > FRAME_FOR_BEAN(24 * 48)) {
+        this.center_rotation_container.rotation.set(0, 0, Math.PI / 3 - 3 * Math.PI / 3 *asmoothstep(FRAME_FOR_BEAN(24*48), FRAME_FOR_BEAN(12), frame));
+      }
     }
   }
 
