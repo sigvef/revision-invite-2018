eed012a32bce7bd98332646f642eb19f213f4deb
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-11-27 02:03:52 +0100

    Update smchmapple timings

--- src/schmapple.js
+++ src/schmapple.js
@@ -3,6 +3,8 @@
   const TEXT_IMAGE_HEIGHT = 64;
   const CANVAS_SCALER = 0.05;
 
+    const beanOffset = 48 * 4;
+
   class schmapple extends NIN.Node {
     constructor(id) {
       super(id, {
@@ -167,33 +169,36 @@
       this.ctx.fillStyle = '#111';
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
       this.ctx.save();
-      for(let i = 0; i < this.texts.length; i++) {
-        const row = this.texts[i];
-        this.ctx.save();
-        this.ctx.translate(-row.offset, 0);
-        let sentenceOffset = 0;
-        for(let j = 0; j < row.length * 2; j++) {
+      const alphaMultiplier = (this.frame - 1126) / (1627 - 1126);
+      if(BEAN < 612) {
+        for(let i = 0; i < this.texts.length; i++) {
+          const row = this.texts[i];
           this.ctx.save();
-          const sentence = row[j % row.length];
-          this.ctx.globalAlpha = 0.2 + sentence.throb * 0.8;
-          const half = (sentence.canvas.width * CANVAS_SCALER + row.padding) / 2;
-          sentenceOffset += half;
-          const scale = (1 + sentence.throb * 0.2);
-          this.ctx.translate(sentenceOffset, 6 + 10 * i);
-          this.ctx.scale(scale, scale);
-          this.ctx.scale(CANVAS_SCALER, CANVAS_SCALER);
-          this.ctx.drawImage(
-            sentence.canvas,
-            -sentence.canvas.width / 2,
-            -sentence.canvas.height / 2);
-          sentenceOffset += half;
+          this.ctx.translate(-row.offset, 0);
+          let sentenceOffset = 0;
+          for(let j = 0; j < row.length * 2; j++) {
+            this.ctx.save();
+            const sentence = row[j % row.length];
+            this.ctx.globalAlpha = alphaMultiplier * (0.2 + sentence.throb * 0.8);
+            const half = (sentence.canvas.width * CANVAS_SCALER + row.padding) / 2;
+            sentenceOffset += half;
+            const scale = (1 + sentence.throb * 0.2);
+            this.ctx.translate(sentenceOffset, 6 + 10 * i);
+            this.ctx.scale(scale, scale);
+            this.ctx.scale(CANVAS_SCALER, CANVAS_SCALER);
+            this.ctx.drawImage(
+              sentence.canvas,
+              -sentence.canvas.width / 2,
+              -sentence.canvas.height / 2);
+            sentenceOffset += half;
+            this.ctx.restore();
+          }
           this.ctx.restore();
         }
-        this.ctx.restore();
       }
 
 
-      if(BEAN >= 420) {
+      if(BEAN >= 420 + beanOffset) {
         this.ctx.save();
         let scaler = 65 / this.revisionLogoOuter.width;
         this.ctx.translate(160 / 2, 90 / 2);
@@ -222,36 +227,41 @@
       this.ctx.fillStyle = 'white';
       this.ctx.beginPath();
 
-      const widthT = (this.frame - FRAME_FOR_BEAN(420)) / (FRAME_FOR_BEAN(420+ 24) - FRAME_FOR_BEAN(420));
+      const widthT = (this.frame - FRAME_FOR_BEAN(420 + beanOffset)) / (FRAME_FOR_BEAN(420+ 24 + beanOffset) - FRAME_FOR_BEAN(420 + beanOffset));
       let outerWidth = easeOut(31, 26, widthT);
       let innerWidth = easeOut(21, 26, widthT);
       let secondSmallestWidth = easeOut(17.6, 5, widthT);
       let smallestWidth = easeOut(0, 5, widthT);
 
-      const openingT = (this.frame - FRAME_FOR_BEAN(240)) / (FRAME_FOR_BEAN(240 + 24) - FRAME_FOR_BEAN(240));
-      const openingT2 = (this.frame - FRAME_FOR_BEAN(240 + 3)) / (FRAME_FOR_BEAN(240 + 3 + 24) - FRAME_FOR_BEAN(240 + 3));
+      const openingT = (this.frame - FRAME_FOR_BEAN(240 + beanOffset)) / (FRAME_FOR_BEAN(240 + 24 + beanOffset) - FRAME_FOR_BEAN(240 + beanOffset));
+      const openingT2 = (this.frame - FRAME_FOR_BEAN(240 + 3 + beanOffset)) / (FRAME_FOR_BEAN(240 + 3 + 24 + beanOffset) - FRAME_FOR_BEAN(240 + 3 + beanOffset));
       outerWidth = elasticOut(0, outerWidth, 1, openingT);
       innerWidth = elasticOut(0, innerWidth, 1, openingT);
       secondSmallestWidth = elasticOut(0, secondSmallestWidth, 1, openingT2);
       smallestWidth = elasticOut(0, smallestWidth, 1, openingT2);
 
-      const smallThrobT = (this.frame - FRAME_FOR_BEAN(240)) / (FRAME_FOR_BEAN(240 + 48) - FRAME_FOR_BEAN(240 + 24));
-      secondSmallestWidth = easeOut(secondSmallestWidth * 2, 8, smallThrobT);
+      const smallThrobT = (this.frame - FRAME_FOR_BEAN(240 + beanOffset)) / (FRAME_FOR_BEAN(240 + 48 + beanOffset) - FRAME_FOR_BEAN(240 + 24 + beanOffset));
 
-      if(BEAN >= 240 + 24) {
+      if(BEAN < 612) {
+        secondSmallestWidth = easeOut(secondSmallestWidth * 2, 8, smallThrobT);
+      }
+
+      if(BEAN >= 240 + 24 + beanOffset && BEAN < 612) {
         secondSmallestWidth += this.kickThrob * 8;
       }
 
-      this.ctx.translate(160 / 2, 90 / 2);
-      this.ctx.moveTo(outerWidth, 0);
-      this.ctx.arc(0, 0, outerWidth, 0, Math.PI * 2);
-      this.ctx.moveTo(innerWidth, 0);
-      this.ctx.arc(0, 0, innerWidth, 0, Math.PI * 2);
-      this.ctx.moveTo(secondSmallestWidth, 0);
-      this.ctx.arc(0, 0, secondSmallestWidth, 0, Math.PI * 2);
-      this.ctx.moveTo(smallestWidth, 0);
-      this.ctx.arc(0, 0, smallestWidth, 0, Math.PI * 2);
-      this.ctx.fill('evenodd');
+      if(BEAN < 624) {
+        this.ctx.translate(160 / 2, 90 / 2);
+        this.ctx.moveTo(outerWidth, 0);
+        this.ctx.arc(0, 0, outerWidth, 0, Math.PI * 2);
+        this.ctx.moveTo(innerWidth, 0);
+        this.ctx.arc(0, 0, innerWidth, 0, Math.PI * 2);
+        this.ctx.moveTo(secondSmallestWidth, 0);
+        this.ctx.arc(0, 0, secondSmallestWidth, 0, Math.PI * 2);
+        this.ctx.moveTo(smallestWidth, 0);
+        this.ctx.arc(0, 0, smallestWidth, 0, Math.PI * 2);
+        this.ctx.fill('evenodd');
+      }
 
       this.ctx.restore();
 
