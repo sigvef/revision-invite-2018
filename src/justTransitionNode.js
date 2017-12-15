bc2147bd273bd4ee92b551add1267ffa8803f60c
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-12-15 23:27:39 +0100

    Move just dropdown a bit earlier

--- src/justTransitionNode.js
+++ src/justTransitionNode.js
@@ -24,27 +24,37 @@
     }
 
     update(frame) {
-      const t = (frame - FRAME_FOR_BEAN(18 * 48)) / (FRAME_FOR_BEAN(18.25 * 48) - FRAME_FOR_BEAN(18 * 48));
+      const firstDrumBeat = FRAME_FOR_BEAN(48 * 18 - 6);
+      const secondDrumBeat = FRAME_FOR_BEAN(48 * 18 - 3);
+      const atariSceneStart = FRAME_FOR_BEAN(48 * 18);
+      const animationFinished = FRAME_FOR_BEAN(48 * 18.25);
+
+      const rectPositionY = easeOut(
+        easeOut(-2, 0, (frame - firstDrumBeat) / (secondDrumBeat - firstDrumBeat)),
+        9, (frame - atariSceneStart) / (animationFinished - atariSceneStart));
+
       this.textCtx.save();
       this.textCtx.scale(GU, GU);
       this.textCtx.font = '1pt schmalibre';
       this.textCtx.textAlign = 'center';
+
+
       this.textCtx.textBaseline = 'middle';
       this.textCtx.fillStyle = '#ff0000';
-      this.textCtx.fillRect(0, 0, 16, easeOut(-2, 9, t));
+      this.textCtx.fillRect(0, 0, 16, rectPositionY);
       this.textCtx.fillStyle = '#0000ff';
-      this.textCtx.fillRect(0, easeOut(0, 11, t), 16, 9);
+      this.textCtx.fillRect(0, 2.0 + rectPositionY, 16, 9);
       this.textCtx.fillStyle = '#77e15d';
-      this.textCtx.fillRect(0, easeOut(-2, 9, t), 16, 2);
+      this.textCtx.fillRect(0, rectPositionY, 16, 2);
       this.textCtx.fillStyle = 'white';
-      this.textCtx.fillText('JUST', 8, easeOut(-0.6, 10.165, t));
+      this.textCtx.fillText('JUST', 8, 0.94 + rectPositionY);//    easeOut(-0.6, 10.165, t));
       this.textCtx.restore();
       this.textTexture.needsUpdate = true;
       this.uniforms.A.value = this.inputs.A.getValue();
       this.uniforms.B.value = this.inputs.B.getValue();
       this.uniforms.text.value = this.textTexture;
 
-      if (t == 1) {
+      if (frame === atariSceneStart) {
         demo.nm.nodes.bloom.opacity = 0.5;
       }
     }
