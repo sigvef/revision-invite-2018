eb89e67319db19d42f260e38891b2845a223b932
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2017-12-17 16:38:08 +0100

    Reconnect graph with new overlay

--- src/CccBlendNode.js
+++ src/CccBlendNode.js
@@ -1,11 +1,17 @@
 (function(global) {
   class CccBlendNode extends NIN.ShaderNode {
     constructor(id, options) {
+      options.inputs = {
+        A: new NIN.TextureInput(),
+        B: new NIN.TextureInput(),
+      };
       super(id, options);
     }
 
     update(frame) {
       this.uniforms.frame.value = frame;
+      this.uniforms.A.value = this.inputs.A.getValue();
+      this.uniforms.B.value = this.inputs.B.getValue();
     }
   }
 
