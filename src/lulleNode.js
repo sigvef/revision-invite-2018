d4e32e2e43ac739aef877e7efe4f5f86bfbb4db5
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-12-17 04:01:32 +0100

    Setup lulle scene for animation etc

--- src/lulleNode.js
+++ src/lulleNode.js
@@ -11,6 +11,7 @@
       this.uniforms.tDiffuse.value = this.inputs.bg.getValue();
       this.uniforms.BEAN.value = BEAN;
       this.uniforms.BEAT.value = BEAT ? 1 : 0;
+      demo.nm.nodes.bloom.opacity = 0.5;
     }
   }
 
