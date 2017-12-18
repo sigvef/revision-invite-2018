738533e7a87a108c49d35d4ad03bd14824e4d036
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2017-12-19 00:16:03 +0100

    Update text on tentacle

--- src/cccOverlay.js
+++ src/cccOverlay.js
@@ -35,14 +35,13 @@
       this.ctx.save();
       this.ctx.scale(GU, GU);
 
-      if(frame > 7300) {
+      if(frame > 7320) {
         const t = lerp(
           0,
-          lerp(1, 0, (frame - 7512) / 20),
-          (frame - 7377) / 20
+          lerp(1, 0, (frame - 7532) / 20),
+          (frame - 7397) / 20
         );
 
-        // NO RIBBONS
         this.ctx.fillStyle = 'rgba(8, 10, 12, .6)';
         this.ctx.beginPath();
         this.ctx.moveTo(16, 9);
@@ -59,7 +58,57 @@
         this.ctx.font = '0.7pt schmalibre-light';
         this.ctx.fillStyle = '#c2c2a0';
 
-        this.ctx.fillText('NO PARTICLES', -2.7, easeIn(1.5, -3.5, t));
+        this.ctx.fillText('JUST REVISION', -2.7, easeIn(1.5, -3.5, t));
+        this.ctx.restore();
+      } else {
+        const t = lerp(
+          0,
+          lerp(1, 0, (frame - 7181) / 20),
+          (frame - 7030) / 20
+        );
+
+        this.ctx.fillStyle = 'rgba(8, 10, 12, .6)';
+        this.ctx.beginPath();
+        this.ctx.moveTo(16, 9);
+        this.ctx.lineTo(easeIn(16, 9, t), 9);
+        this.ctx.lineTo(16, easeIn(9, 2, t));
+        this.ctx.lineTo(16, 9);
+        this.ctx.fill();
+
+        this.ctx.save();
+        this.ctx.translate(16, 9);
+        this.ctx.rotate(-Math.PI / 4);
+        this.ctx.textAlign = 'left';
+        this.ctx.textBaseline = 'middle';
+        this.ctx.font = '0.7pt schmalibre-light';
+        this.ctx.fillStyle = '#c2c2a0';
+
+        this.ctx.fillText('JUST REVISION', -2.7, easeIn(1.5, -3.5, t));
+        this.ctx.restore();
+      }
+      if(frame > 7300) {
+        const t = lerp(
+          0,
+          lerp(1, 0, (frame - 7512) / 20),
+          (frame - 7377) / 20
+        );
+        // NO GRAS || NO STARS
+        this.ctx.fillStyle = 'rgba(8, 10, 12, .6)';
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, 0);
+        this.ctx.lineTo(easeIn(0, 7, t), 0);
+        this.ctx.lineTo(0, easeIn(0, 7, t));
+        this.ctx.lineTo(0, 0);
+        this.ctx.fill();
+
+        this.ctx.save();
+        this.ctx.rotate(-Math.PI / 4);
+        this.ctx.textAlign = 'left';
+        this.ctx.textBaseline = 'middle';
+        this.ctx.font = '0.7pt schmalibre-light';
+        this.ctx.fillStyle = '#c2c2a0';
+
+        this.ctx.fillText('NO PARTICLES', -2.2, easeIn(-1.5, 3.5, t));
         this.ctx.restore();
       } else {
         const t = lerp(
